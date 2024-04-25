a99469a8619d1c24807483e47d56d497caa46251
Riccardo Roveri (ENVY) <ricky.roveri@gmail.com>
2024-04-25 16:55:50 +0200

    terraform: add storage account

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,10 @@ resource "azurerm_resource_group" "rg" {
   location = "westeurope"
 }
 
+resource "azurerm_storage_account" "storage_account" {
+  name                     = var.azurerm_storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,9 @@ variable "azurerm_resource_group_name" {
   description = "Name of the resource group."
 }
 
+variable "azurerm_storage_account_name" {
+  type        = string
+  default     = "shodantfstorageaccount"
+  description = "Name of the storage account."
+
+}
