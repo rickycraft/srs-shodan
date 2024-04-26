174bf33dfaebc25a7d654f32f3a7f4ea7ac61186
Riccardo <ricky.roveri@gmail.com>
2024-04-26 15:36:11 +0200

    function: producer add eventgrid

--- functions/function_app.py
+++ functions/function_app.py
@@ -1,6 +1,10 @@
 import logging
+import os
+from datetime import datetime
 
 import azure.functions as func
+from azure.core.credentials import AzureKeyCredential
+from azure.eventgrid import EventGridEvent, EventGridPublisherClient
 
 app = func.FunctionApp()
 
@@ -14,8 +18,38 @@ def ShodanConsumer(azeventgrid: func.EventGridEvent):
 @app.route(route="publish", auth_level=func.AuthLevel.ANONYMOUS)
 def ShodanProducer(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    logging.info(req.get_json())
-    return func.HttpResponse(
-        "This HTTP triggered function executed successfully.",
-        status_code=200
-        )
+    try:
+        logging.info(req.get_json())
+        data = req.get_json()
+        send_event(data)
+        return func.HttpResponse(
+            "This HTTP triggered function executed successfully.",
+            status_code=200
+            )
+    except Exception as err:
+        return func.HttpResponse(
+            f"Error: {err}",
+            status_code=500
+            )
+
+def send_event(data):
+    key = os.environ["EVENTGRID_KEY"]
+    endpoint = os.environ["EVENTGRID_ENDPOINT"]
+    event = {
+      "data": data,
+      "timestamp": datetime.now()
+    }
+
+    credential = AzureKeyCredential(key)
+    client = EventGridPublisherClient(endpoint, credential)
+
+    # Create an EventGridEvent object
+    event_grid_event = EventGridEvent(
+        subject="shodan/subject",
+        event_type="Shodan.Test",
+        data=event,
+        data_version="1.1"
+    )
+
+    # Send the event
+    client.send(event_grid_event)
