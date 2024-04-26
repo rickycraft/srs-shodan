e98ee98e4a4efbf9c91de672622a9632a2f44b8e
Riccardo <ricky.roveri@gmail.com>
2024-04-26 16:03:59 +0200

    functions: consumer fetch ex webhook

--- functions/function_app.py
+++ functions/function_app.py
@@ -1,9 +1,11 @@
 import logging
+import urllib.request
 
 import azure.functions as func
 import producer
 
 app = func.FunctionApp()
+webhook_url = "https://webhook.rroveri.com/azure"
 
 @app.function_name(name="ShodanProducer")
 @app.route(route="publish", auth_level=func.AuthLevel.ANONYMOUS)
@@ -27,4 +29,10 @@ def ShodanProducer(req: func.HttpRequest) -> func.HttpResponse:
 @app.event_grid_trigger(arg_name="azeventgrid")
 def ShodanConsumer(azeventgrid: func.EventGridEvent):
     logging.info('Python EventGrid trigger processed an event')
-    logging.info(azeventgrid.get_json())
+    data = azeventgrid.get_json()
+    logging.info(data)
+
+    req = urllib.request.Request(webhook_url, data=data, method='POST')
+    with urllib.request.urlopen(req) as response:
+        res = response.read()
+        logging.info(res)
