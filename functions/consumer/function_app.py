5a3ea8d34cb1f847842506db7f72cabccb1226fa
Riccardo <ricky.roveri@gmail.com>
2024-04-26 14:32:54 +0200

    functions: add shodan test producer

--- functions/consumer/function_app.py
+++ functions/consumer/function_app.py
@@ -4,7 +4,18 @@ import azure.functions as func
 
 app = func.FunctionApp()
 
+@app.function_name(name="ShodanConsumer")
 @app.event_grid_trigger(arg_name="azeventgrid")
 def ShodanConsumer(azeventgrid: func.EventGridEvent):
     logging.info('Python EventGrid trigger processed an event')
     logging.info(azeventgrid.get_json())
+
+@app.function_name(name="ShodanProducer")
+@app.route(route="publish", auth_level=func.AuthLevel.ANONYMOUS)
+def ShodanProducer(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    logging.info(req.get_json())
+    return func.HttpResponse(
+        "This HTTP triggered function executed successfully.",
+        status_code=200
+        )
