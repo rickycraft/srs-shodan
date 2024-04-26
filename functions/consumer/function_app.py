94bd4d82b1025c9fa6ddd1bfd59d698cb794f50c
Riccardo <ricky.roveri@gmail.com>
2024-04-26 14:08:48 +0200

    function: rename consumer file

---
+++ functions/consumer/function_app.py
@@ -0,0 +1,10 @@
+import logging
+
+import azure.functions as func
+
+app = func.FunctionApp()
+
+@app.event_grid_trigger(arg_name="azeventgrid")
+def ShodanConsumer(azeventgrid: func.EventGridEvent):
+    logging.info('Python EventGrid trigger processed an event')
+    logging.info(azeventgrid.get_json())
