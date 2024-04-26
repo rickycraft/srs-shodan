a84e1cabf540b5e998750f97507445bf5fdd6551
Riccardo <ricky.roveri@gmail.com>
2024-04-26 15:41:15 +0200

    function: move sed_event method

--- functions/function_app.py
+++ functions/function_app.py
@@ -8,30 +8,6 @@ from azure.eventgrid import EventGridEvent, EventGridPublisherClient
 
 app = func.FunctionApp()
 
-@app.function_name(name="ShodanConsumer")
-@app.event_grid_trigger(arg_name="azeventgrid")
-def ShodanConsumer(azeventgrid: func.EventGridEvent):
-    logging.info('Python EventGrid trigger processed an event')
-    logging.info(azeventgrid.get_json())
-
-@app.function_name(name="ShodanProducer")
-@app.route(route="publish", auth_level=func.AuthLevel.ANONYMOUS)
-def ShodanProducer(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    try:
-        logging.info(req.get_json())
-        data = req.get_json()
-        send_event(data)
-        return func.HttpResponse(
-            "This HTTP triggered function executed successfully.",
-            status_code=200
-            )
-    except Exception as err:
-        return func.HttpResponse(
-            f"Error: {err}",
-            status_code=500
-            )
-
 def send_event(data):
     key = os.environ["EVENTGRID_KEY"]
     endpoint = os.environ["EVENTGRID_ENDPOINT"]
@@ -53,3 +29,27 @@ def send_event(data):
 
     # Send the event
     client.send(event_grid_event)
+
+@app.function_name(name="ShodanProducer")
+@app.route(route="publish", auth_level=func.AuthLevel.ANONYMOUS)
+def ShodanProducer(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    try:
+        logging.info(req.get_json())
+        data = req.get_json()
+        send_event(data)
+        return func.HttpResponse(
+            "This HTTP triggered function executed successfully.",
+            status_code=200
+            )
+    except Exception as err:
+        return func.HttpResponse(
+            f"Error: {err}",
+            status_code=500
+            )
+
+@app.function_name(name="ShodanConsumer")
+@app.event_grid_trigger(arg_name="azeventgrid")
+def ShodanConsumer(azeventgrid: func.EventGridEvent):
+    logging.info('Python EventGrid trigger processed an event')
+    logging.info(azeventgrid.get_json())
