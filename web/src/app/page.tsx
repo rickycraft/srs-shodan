1d996ec0a8b9dbf97fd88966dcbfc46f0b00df1b
Andreaise <andrea.isernio@gmail.com>
2024-05-24 02:23:48 +0200

    primoLoginOauth

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,32 +1,34 @@
-import Link from "next/link"
+'use client';
+import Link from 'next/link';
+import React from 'react';
+import { signIn } from 'next-auth/react';
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          <span className="text-[hsl(280,100%,70%)]">SHODAN Monitoring</span> WebApp
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
+          <button
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
+            onClick={() => signIn('github')}
           >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
+            <h3 className="text-2xl font-bold">Login →</h3>
             <div className="text-lg">
-              Progetto SRS
+              OAuth di GitHub
+              <img src="/logoGitHub.png" alt="GitHub OAuth Login" />
             </div>
-          </Link>
+          </button>
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
             href="https://create.t3.gg/en/introduction"
             target="_blank"
           >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
+            <h3 className="text-2xl font-bold">Repo Github   →</h3>
             <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
+              Repository GitHub del Progetto di Scalable and Reliable Services M
             </div>
           </Link>
         </div>
