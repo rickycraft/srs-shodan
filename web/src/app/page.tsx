7dd0f904076adfd8ae60f6893d42ccf001f11955
Andreaise <andrea.isernio@gmail.com>
2024-06-22 18:14:37 +0200

    added button to go on dashboard and welcome sign with username

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,12 @@
 import Link from 'next/link'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '~/server/auth'
+import { User } from 'next-auth'
 
 export default async function HomePage() {
+  const session = await getServerSession(authOptions)
+  const user: User | null = session ? session.user : null
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
@@ -8,7 +14,15 @@ export default async function HomePage() {
           <span className="text-[hsl(280,100%,70%)]">SHODAN Monitoring</span>{' '}
           WebApp
         </h1>
+        {user && <div className="mt-4 text-lg">Benvenuto, {user.name}!</div>}
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="/dashboard"
+          >
+            <h3 className="text-2xl font-bold">Go to Dashboard →</h3>
+            <div className="text-lg">Vai alla dashboard</div>
+          </Link>
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
             href="https://github.com/rickycraft/srs-shodan"
